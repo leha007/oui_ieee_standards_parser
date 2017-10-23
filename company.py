ff8d49a29411e0835d8d415bb8c3c88a0efbd982
leha007 <alexeys007@gmail.com>
2017-10-23 21:17:51 +0300

    Initial commit.

---
+++ company.py
@@ -0,0 +1,22 @@
+class Company():
+    def __init__(self, name):
+        self.name = name.strip()
+        self.cnt = 0
+        self.hex = ''
+        self.base = ''
+        self.hrdw_addr_array = []
+
+    def add_hex_mac(self, mac):
+        self.hex = mac.strip()
+
+    def add_base_mac(self, mac):
+        self.base = mac.strip()
+        self.trigger()
+
+    def trigger(self):
+        if self.hex != '' and self.base != '':
+            self.hrdw_addr_array.append(self.hex + '+' + self.base)
+            self.cnt = self.cnt + 1
+
+        self.hex = ''
+        self.base = ''
